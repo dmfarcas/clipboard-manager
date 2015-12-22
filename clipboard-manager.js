b8b847854c225301a8605b9d72f4d70c18961950
dmfarcas <dan@farcas.me>
2015-12-22 16:44:27 +0200

    ADD: sending clipboard to renderer

---
+++ clipboard-manager.js
@@ -0,0 +1,5 @@
+var app = angular.module("clipboardManager", []);
+
+app.controller("mainController", ['$scope',  function($scope) {
+  $scope.number = 'Testing!';
+}]);
--- main.js
+++ main.js
@@ -4,24 +4,19 @@ const app = electron.app;  // Module to control application life.
 const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
 const clipboard = require('electron').clipboard;
 const globalShortcut = electron.globalShortcut;
+const ipcMain = require('electron').ipcMain;
 const Tray = electron.Tray;
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 var appIcon = null;
 
-// Quit when all windows are closed.
+// Seems to be needed to keep the application alive.
 app.on('window-all-closed', function() {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  // if (process.platform != 'darwin') {
-  //   app.quit();
-  // }
+  // woopwoopwoop
 });
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-
+// Create a window!
 function createWindow() {
   mainWindow = new BrowserWindow({width: 800,
     height: 600,
@@ -34,7 +29,8 @@ function createWindow() {
   		});
 }
 
-
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
 app.on('ready', function() {
   // Create the browser window.
   createWindow();
@@ -42,6 +38,9 @@ app.on('ready', function() {
   // Create a tray icon
   appIcon = new Tray('images/logo.png');
   appIcon.setToolTip('This is my application.');
+
+
+
 
   // Show and hide the application
   appIcon.on('click', function() {
@@ -57,12 +56,10 @@ app.on('ready', function() {
   }
   });
 
-  // Load the index.html of the app.
-
-
-
   // Unfortunately, because of an Electron limitation, CTRL + C cannot be captured because it overwrites the system default copy shortcut.
   var ret = globalShortcut.register('CmdorCtrl+Shift+C', function() {
+    mainWindow.webContents.send('copied', clipboard.readText());
+    // sends the clipboard to the renderer
     console.log(clipboard.readText());
   });
   if (!ret) {
@@ -76,7 +73,7 @@ app.on('ready', function() {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    // mainWindow = null;
+    mainWindow = null;
 
   });
 
