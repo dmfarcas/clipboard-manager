a75a6298e499ae7728f52bd4e931a95bbba8dac1
dmfarcas <dan@farcas.me>
2016-01-08 23:00:35 +0200

    Persistent data working, sorted using unix time. Some undefined variable issues to be fixed.

--- assets/index.js
+++ assets/index.js
@@ -3,7 +3,7 @@
 var LinvoDB = require("linvodb3");
 LinvoDB.defaults.store = { db: require("medeadown") };
 LinvoDB.dbPath = process.cwd();
-var Doc = new LinvoDB("doc", { /* schema, can be empty */ })
+var Doc = new LinvoDB("doc", { text: String, time: Date})
 var doc = new Doc();
 
 // receiving events from main process
@@ -12,25 +12,38 @@ appendRow(message);
 });
 
 
-function appendRow(text) {
-  const clipboard = require('electron').clipboard;
-  var table=document.getElementById('dasTable');
-  var row=table.insertRow(1);
-  var newCell=row.insertCell(0);
-  var newCell2=row.insertCell(1);
-  newCell.setAttribute('contenteditable', 'true');
-  // It works, hooray.
-  newCell.setAttribute('onclick', 'copyText();');
-  var newText  = document.createTextNode(text);
-  newCell.appendChild(newText);
-  newCell2.innerHTML = moment().format("HH:MM:ss");
-
-  Doc.save([ doc, { paste: text } ], function(err, docs) {
-
+// Initial database loading
+Doc.find({}).sort({time: 1}).exec(function (err, docs) {
+  if(err) {
+    console.error("Cannot load database.")
+  }
+  for (var i = 0; i < docs.length; i++) {
+    populateTable(docs[i].text, moment.unix(docs[i].time).format("HH:MM:ss"));
+  }
 });
 
+
+function appendRow(text) {
+  var time = moment().unix();
+  populateTable(text, moment.unix(time).format("HH:MM:ss"))
+  Doc.save([ doc, { text: text, time: time } ], function(err, docs) {
+    if(err) {
+      console.error("Something went wrong while saving data.");
+    }
+  });
 }
 
+function populateTable(text, time) {
+  var table=document.getElementById('dasTable');
+  var row=table.insertRow(1);
+  var textCell=row.insertCell(0);
+  var timeCell=row.insertCell(1);
+  textCell.setAttribute('contenteditable', 'true');
+  textCell.setAttribute('onclick', 'copyText();');
+  var newText  = document.createTextNode(text);
+  textCell.appendChild(newText);
+  timeCell.innerHTML = time;
+}
 
 function copyText() {
   document.execCommand('selectAll',false,null);
