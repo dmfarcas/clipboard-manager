49c820d4e05702605beb23e92b405dce98b95c43
Farcas <dan.farcas@assist.ro>
2016-07-05 10:25:15 +0300

    Do not open dev tools on start.

--- main.js
+++ main.js
@@ -167,9 +167,6 @@ app.on('ready', function() {
   });
 
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
     // Dereference the window object, usually you would store windows
